326fdbec572fecb9f4ff88b12c1505cfe93882d5
qiagu <guqiang01@gmail.com>
2021-01-19 19:42:23 -0800

    update conda recipes for cellpose and opencv

--- conda/opencv-python-headless/build.sh
+++ conda/opencv-python-headless/build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 if [ `uname` == Darwin ]; then
-    pip install https://files.pythonhosted.org/packages/b8/46/adc2e1e0e94c8a61a70f6be25237ff01d5e97bcc292f15756f11ea75e405/opencv_python-4.4.0.42-cp37-cp37m-macosx_10_13_x86_64.whl
+    pip install https://files.pythonhosted.org/packages/4a/c3/6a2cf67067e6d8c5aadb7cbae891a774cfc9e63ef35f7e8e74fa6636a9ef/opencv_python_headless-4.5.1.48-cp37-cp37m-macosx_10_13_x86_64.whl
 fi
 
 if [ `uname` == Linux ]; then
-    pip install https://files.pythonhosted.org/packages/fa/e3/7ed67a8f3116113a364671fb4142c446dd804c63f3d9df5c11168a1e4dbb/opencv_python-4.4.0.42-cp37-cp37m-manylinux2014_x86_64.whl
+    pip install https://files.pythonhosted.org/packages/6d/6d/92f377bece9b0ec9c893081dbe073a65b38d7ac12ef572b8f70554d08760/opencv_python_headless-4.5.1.48-cp37-cp37m-manylinux2014_x86_64.whl
 fi
